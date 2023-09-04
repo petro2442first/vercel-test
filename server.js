c027b4970ee76750d9693ee697fb474b8dd323ab
Danylo Petrenko <mrb13022001@gmail.com>
2023-09-04 13:50:57 +0300

    init

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+global.__rootDir = __dirname;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "/client/public")));
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/client/public/index.html`);
+});
+app.get("*", (req, res) => {
+  res.sendFile(`${__dirname}/client/public/index.html`);
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 11111;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
